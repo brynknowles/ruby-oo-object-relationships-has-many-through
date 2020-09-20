257b8b70074e11400f41b0107df088a57b045888
brynknowles <bryn.knowles@gmail.com>
2020-09-20 13:33:39 -0400

    lab completed

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 class Waiter
+    attr_accessor :name, :age
+
+    @@all = []
+
+    def initialize(name, years)
+        @name = name
+        @years = years 
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def meals
+        Meal.all.select { |meal| meal.waiter == self}
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def best_tipper
+        # self.meals.max_by { |meal| meal.tip}.customer
+        best_tip = meals.max_by { |meal| meal.tip}
+        best_tip.customer
+    end
 
 end
